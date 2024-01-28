d0cae996709bcba802f80f66b53ae915034f6add
Olga Shalakhina <osshalakhina@gmail.com>
2024-01-28 17:01:30 +0200

    character details

--- src/pages/CharacterDetails/index.tsx
+++ src/pages/CharacterDetails/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { API_REST } from "../../variables/urls";
 import { useParams } from "react-router-dom";
 import { Character } from "../../types";
+import "scss/index.scss";
 
 const CharacterDetails = () => {
   const { characterId } = useParams();
@@ -15,10 +16,23 @@ const CharacterDetails = () => {
   useEffect(() => {
     getCharacter();
   }, []);
-  return <div>
-
-    <h1>{character?.name}</h1>
-  </div>;
+  return (
+    <div className="personal">
+      <h1 className="personalName">{character?.name}</h1>
+      <img
+        className="personalPhoto"
+        src={character?.image}
+        alt={character?.name}
+      />
+      <div className="personalInfo">
+        <p>{character?.species}</p>
+        <p>{character?.gender}</p>
+        <p>{character?.status}</p>
+        <p>{character?.location.name}</p>
+      </div>
+      <p>{character?.created}</p>
+    </div>
+  );
 };
 
 export default CharacterDetails;
