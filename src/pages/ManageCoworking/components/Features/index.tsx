8e982de8f6b992a7cb7bf48ee3885e0f8702dea2
Olga Shalakhina <osshalakhina@gmail.com>
2024-02-06 20:47:47 +0200

    languages

--- src/pages/ManageCoworking/components/Features/index.tsx
+++ src/pages/ManageCoworking/components/Features/index.tsx
@@ -1,11 +1,84 @@
-import React from 'react';
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import TextField from "components/UI/TextField";
+import { GeneralSchema, generalSchema } from "helpers/validations";
+import { useManageContext } from "../../context";
 
 const Features = () => {
-  return (
-    <div>
+  const { setData, setCurrentStep } = useManageContext();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<GeneralSchema>({});
 
-      Features form
-    </div>
+  const onSubmit = (data: GeneralSchema) => {
+    setData((prevState) => ({ ...prevState, general: data }));
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      style={{ maxWidth: 500, padding: 40 }}
+    >
+      <h1>Features</h1>
+
+      <Controller
+        control={control}
+        render={({
+          field: { value, onChange, ...inputProps },
+          fieldState: { error },
+        }) => {
+          return (
+            <TextField
+              label={"Enter name"}
+              error={error?.message}
+              value={value}
+              onChange={({ target: { value } }) => {
+                let newValue =
+                  value.charAt(0).toUpperCase() +
+                  value.substring(1, value.length);
+                onChange(newValue);
+              }}
+              {...inputProps}
+            />
+          );
+        }}
+        name="name"
+      />
+
+      {/* <Controller
+        control={control}
+        render={({ field }) => (
+          <TextField label="Price" type="number" {...field} />
+        )}
+        name="price"
+      />
+
+      <Controller
+        control={control}
+        render={({ field }) => <TextField label="Tag" {...field} />}
+        name="tag"
+      /> */}
+
+      <br />
+
+      <button
+        style={{
+          padding: 20,
+          marginTop: 10,
+          width: 200,
+          borderRadius: 8,
+          color: "white",
+          backgroundColor: `var(--color-primary)`,
+          marginBottom: 10,
+        }}
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
   );
 };
 
--- src/pages/ManageCoworking/components/Languages/index.tsx
+++ src/pages/ManageCoworking/components/Languages/index.tsx
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { useManageContext } from "../../context";
+import { Steps } from "types";
 
-const Languages = () => {
+function LanguageSelect() {
+  const { control, handleSubmit } = useForm();
+  const { setData, setCurrentStep } = useManageContext();
+  const [selectedLanguage, setSelectedLanguage] = useState("");
+
+  const onSubmit = (data: any) => {
+    setData((prevState) => ({ ...prevState, language: data }));
+    setCurrentStep(Steps.FEATURES);
+  };
+
   return (
-    <div>
-
-      <select name="" id=""></select>
-      Languages form
-    </div>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      style={{ maxWidth: 500, padding: 40 }}
+    >
+      <h1>Language</h1>
+      <Controller
+        name="language"
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <select
+            {...field}
+            style={{
+              padding: 10,
+              height: 42,
+              marginTop: 10,
+              width: 200,
+              borderRadius: 8,
+            }}
+            onChange={(e) => setSelectedLanguage(e.target.value)}
+          >
+            <option value="">Choose language</option>
+            <option value="украинский">Українська</option>
+            <option value="английский">English</option>
+            <option value="иврит">עִבְרִית</option>
+          </select>
+        )}
+      />
+      <br />
+      <button
+        disabled={!selectedLanguage}
+        style={{
+          padding: 20,
+          marginTop: 10,
+          width: 200,
+          borderRadius: 8,
+          color: "white",
+          backgroundColor: selectedLanguage
+            ? `var(--color-primary)`
+            : "rgba(66, 66, 255, 0.334)",
+          marginBottom: 10,
+        }}
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
   );
-};
+}
 
-export default Languages;
+export default LanguageSelect;
