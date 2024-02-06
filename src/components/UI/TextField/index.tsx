82280bb7135975c0a6609fb1e2d7db82244841ed
Olga Shalakhina <osshalakhina@gmail.com>
2024-02-06 20:50:04 +0200

    languages

--- src/components/UI/TextField/index.tsx
+++ src/components/UI/TextField/index.tsx
@@ -1,44 +1,49 @@
-import React, {ChangeEvent, forwardRef, ForwardRefRenderFunction} from 'react';
-import {Typography} from "components/Typography";
-import {ColorEnum} from "helpers/types";
-import s from './index.module.scss';
+import React, {
+  ChangeEvent,
+  forwardRef,
+  ForwardRefRenderFunction,
+} from "react";
+import { Typography } from "components/Typography";
+import { ColorEnum } from "helpers/types";
+import s from "./index.module.scss";
 import classNames from "classnames/bind";
 
 const cx = classNames.bind(s);
 
 type TextFieldProps = {
-  value?: string,
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void
-  error?: string
-  placeholder?: string
-  type?: 'text' | 'password',
-  name: string,
-  label?: string
-}
+  value?: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
+  placeholder?: string;
+  type?: "text" | "password";
+  name: string;
+  label?: string;
+};
 
-const TextField: ForwardRefRenderFunction<HTMLInputElement, TextFieldProps> =
-  ({
-     error,
-     type = 'text',
-     label,
-     ...props
-   }, ref) => {
-    return (
-      <div className='flex flex-col'>
-        {label ? <label htmlFor={props.name}>{label}</label> : null}
-        <input
-          ref={ref}
-          id={props.name}
-          className={cx('TextField', {
-            Error: error
-          })}
-          type={type}
-          {...props}
-        />
-        {error && <Typography className='mt-1 mb-2' color={ColorEnum.red}>{error}</Typography>}
-      </div>
-    );
-  };
+const TextField: ForwardRefRenderFunction<HTMLInputElement, TextFieldProps> = (
+  { error, type = "text", label, ...props },
+  ref
+) => {
+  return (
+    <div className="flex flex-col">
+      {label ? <label htmlFor={props.name}>{label}</label> : null}
+      <input
+        ref={ref}
+        id={props.name}
+        className={cx("TextField", {
+          Error: error,
+        })}
+        type={type}
+        {...props}
+      />
+      {error && (
+        <Typography className="mt-1 mb-2" color={ColorEnum.red}>
+          {error}
+        </Typography>
+      )}
+    </div>
+  );
+};
 
 export default forwardRef(TextField);
 
